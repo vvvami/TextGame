1cf225341e482560c6b3cb7aaa47c7d10f2623c2
vami <ayrane.elcapon@gmail.com>
2024-05-22 23:23:54 +0300

    removed debug prints for PlayerInterpreter

--- src/net/vami/game/PlayerInterpreter.java
+++ src/net/vami/game/PlayerInterpreter.java
@@ -11,7 +11,6 @@ public class PlayerInterpreter {
         Scanner scanner = new Scanner(System.in);
         System.out.print(Main.ANSI_BLUE + "> " + Main.ANSI_RESET);
         String fullAction = scanner.nextLine();
-        System.out.println("Full Action String: " + fullAction);
         return actionInput(fullAction);
 
     }
@@ -20,17 +19,14 @@ public class PlayerInterpreter {
     private static boolean actionInput(String input) {
         String[] inputArr = input.toLowerCase()
                 .split("\\s+");
-        System.out.println("Input Array: " + Arrays.toString(inputArr));
         Node node = Game.getCurrentNode();
         Interactable target = null;
         switch (inputArr.length) {
             case 2:
                 target = node.stringToInteractable(inputArr[1]);
-                System.out.println("Case 2");
 
             case 1:
                 if (target == null) {
-                    System.out.println("Target is null");
                     break;
                 }
                 Action action = Action.synonymToAction.get(inputArr[0]);
