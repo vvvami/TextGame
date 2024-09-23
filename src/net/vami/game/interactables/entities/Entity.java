04decfafe689f2b7cece806b5f5ba82e29d2513a
vami <ayrane.elcapon@gmail.com>
2024-09-23 21:05:36 +0300

    removed playtest shit

--- src/net/vami/game/interactables/entities/Entity.java
+++ src/net/vami/game/interactables/entities/Entity.java
@@ -193,7 +193,6 @@ public abstract class Entity extends Interactable {
             int newLevel = this.attributes.levelAttribute + level;
             this.attributes = new Attributes().level(newLevel);
             this.attributes.initialize();
-            System.out.println("Damage: " + this.attributes.damageAttribute);
 
             Game.playSound(Sound.HEAL, 65);
             TextUtil.display(this, "%s grows stronger... %n", this.getName());
