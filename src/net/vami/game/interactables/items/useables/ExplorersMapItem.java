fb0fe6acd3d7af2eedc2defe5d16248ecff5a46c
vami <ayrane.elcapon@gmail.com>
2024-09-25 18:48:04 +0300

    explorer's map now shows node entrances you can move through

--- src/net/vami/game/interactables/items/useables/ExplorersMapItem.java
+++ src/net/vami/game/interactables/items/useables/ExplorersMapItem.java
@@ -4,6 +4,7 @@ import net.vami.game.Game;
 import net.vami.game.interactables.items.Item;
 import net.vami.game.world.Direction;
 import net.vami.game.world.Node;
+import net.vami.game.world.Position;
 import net.vami.util.TextUtil;
 
 import java.util.ArrayList;
@@ -16,5 +17,15 @@ public class ExplorersMapItem extends Item implements UseableItem {
     @Override
     public void onUse() {
         TextUtil.display(this.getOwner(), "You are located at %s.%n", this.getOwner().getPos().toString());
+        Position position = this.getOwner().getPos();
+        for (Direction direction : Direction.values()) {
+            if (Node.getNodeFromPosition(position.add(direction)) == null) {
+                continue;
+            }
+            if (Node.getNodeFromPosition(position.add(direction)).getEntrances()
+                    .contains(direction.getOpposite())) {
+                TextUtil.display(this.getOwner(), "You have an entrance available %sward. %n", direction.toString().toLowerCase());
+            }
+        }
     }
 }
